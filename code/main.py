245985a6e29644d6a316741d27035833944b7629
ilyakolbafk <ivkolb@edu.hse.ru>
2023-04-06 00:41:43 +0300

    вроде теперь все парсит в список заголовков

--- code/main.py
+++ code/main.py
@@ -1,23 +1,64 @@
 import numpy as np
 import pandas as pd
 
-from finder import Finder
+# from finder import Finder
 
 import openpyxl
 
+from textparser import Paragraph
+
+
+def get_on_depth(header, n):
+    if n == 1 or type(header) == Paragraph or len(header.objects) == 0:
+        res = header.__str__()
+        for i in range(n - 1):
+            res = [res]
+        return res
+    else:
+        objects = []
+        for o in header.objects:
+            objects.append(get_on_depth(o, n - 1))
+        return objects
+
 
 def main():
-    game = Finder()
-    a = input()
+    # game = Finder()
+    # a = input()
     from linkgraph import LinkGraph
     sentence_embeddings = LinkGraph().get_link_text()
-    pd.DataFrame(sentence_embeddings).to_excel('texts.xlsx')
+    level_1 = []
+    level_2 = []
+    level_3 = []
+    level_4 = []
+    level_5 = []
+    for i in sentence_embeddings:
+        level_1.append(get_on_depth(i, 1))
+        level_2.append(get_on_depth(i, 2))
+        level_3.append(get_on_depth(i, 3))
+        level_4.append(get_on_depth(i, 4))
+        level_5.append(get_on_depth(i, 5))
+    t = 0
+    for i in level_2:
+        for j in i:
+            t += 1
+    print(t)
+    t = 0
+    for i in level_5:
+        for j in i:
+            for k in j:
+                for p in k:
+                    for m in p:
+                        t += 1
+    print(t)
+    levels = [level_1, level_2, level_3, level_4, level_5]
+    for i, level in enumerate(levels):
+        pd.DataFrame(level).to_excel(f'level_{i+1}.xlsx')
 
 
-# main()
+main()
 
 from linkgraph import LinkGraph
-sentence_embeddings = LinkGraph()
+# sentence_embeddings = LinkGraph()
 # question = ''
 #
 # while question != 0:

938edd7c0739009e34f1c811101352703aaf6ba3
ilyakolbafk <ivkolb@edu.hse.ru>
2023-04-09 21:18:59 +0300

    вроде теперь все парсит в список заголовков

--- code/savingtextdata.py
+++ code/savingtextdata.py
@@ -1,30 +1,20 @@
+import pandas as pd
+
 def save_levels_to_excel():
     from linkgraph import LinkGraph, get_on_depth
     sentence_embeddings = LinkGraph().get_link_text()
-    level_1 = []
-    level_2 = []
-    level_3 = []
-    level_4 = []
-    level_5 = []
-    for i in sentence_embeddings:
-        level_1.append(get_on_depth(i, 1))
-        level_2.append(get_on_depth(i, 2))
-        level_3.append(get_on_depth(i, 3))
-        level_4.append(get_on_depth(i, 4))
-        level_5.append(get_on_depth(i, 5))
-    t = 0
-    for i in level_2:
-        for j in i:
-            t += 1
-    print(t)
-    t = 0
-    for i in level_5:
-        for j in i:
-            for k in j:
-                for p in k:
-                    for m in p:
-                        t += 1
-    print(t)
-    levels = [level_1, level_2, level_3, level_4, level_5]
+    levels = []
+    levels_tokenize = []
+    for j in range(1, 6):
+        levels.append([])
+        levels_tokenize.append([])
+        for i in sentence_embeddings:
+            levels[j - 1].append(get_on_depth(i, j))
+            levels_tokenize[j - 1].append(get_on_depth(i, j, True))
+
     for i, level in enumerate(levels):
         pd.DataFrame(level).to_excel(f'../data_files/level_{i + 1}.xlsx')
+    for i, level in enumerate(levels_tokenize):
+        pd.DataFrame(level).to_excel(f'../data_files/levels_tokenize_{i + 1}.xlsx')
+
+save_levels_to_excel()
---
+++ code/tokenize_data.py
@@ -0,0 +1,19 @@
+from nltk.tokenize import word_tokenize
+import nltk
+from nltk.stem import SnowballStemmer
+
+nltk.download('stopwords')
+nltk.download('punkt')
+from nltk.corpus import stopwords
+
+STOP_WORDS = stopwords.words("russian")
+
+
+def tokenize_data(text):
+    tokens = word_tokenize(text, language="russian")
+    snowball = SnowballStemmer(language="russian")
+    filtered_tokens = []
+    for token in tokens:
+        if token not in STOP_WORDS:
+            filtered_tokens.append(snowball.stem(token))
+    return filtered_tokens

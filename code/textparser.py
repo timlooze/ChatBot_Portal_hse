830356ffb949ea970af0a975b12539cc87575ef9
timonchiks <tim-x10@yandex.ru>
2023-04-11 14:07:11 +0300

    first working files update

--- code/textparser.py
+++ code/textparser.py
@@ -1,3 +1,12 @@
+import nltk
+from nltk import word_tokenize, SnowballStemmer
+from nltk.corpus import stopwords
+
+nltk.download('stopwords')
+nltk.download('punkt')
+STOP_WORDS = stopwords.words("russian")
+
+
 class Header:
     def __init__(self, header_text):
         self.name = 'h'
@@ -130,3 +139,13 @@ def parse(soup, text):
     if len(soup.find_all(True, recursive=False)) == 0:
         current_h1.append(Paragraph(soup))
     return current_h1
+
+
+def tokenize_data(text):
+    tokens = word_tokenize(text, language="russian")
+    snowball = SnowballStemmer(language="russian")
+    filtered_tokens = []
+    for token in tokens:
+        if token not in STOP_WORDS and len(token) > 2:
+            filtered_tokens.append(snowball.stem(token))
+    return filtered_tokens

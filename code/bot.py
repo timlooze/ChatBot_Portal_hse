00c6c4e83b10255f022b9c256ba29d7cf2de674a
ilyakolbafk <ivkolb@edu.hse.ru>
2023-04-12 11:30:44 +0300

    моделька пук пук

--- code/bot.py
+++ code/bot.py
@@ -10,27 +10,27 @@ Telegram bot function producing dialog with the user
 with open('TOKENS.txt') as f:
     TOKEN_TG = f.readline()
 
-bot = telebot.TeleBot(TOKEN_TG)
+BOT = telebot.TeleBot(TOKEN_TG)
 start_text = "Привет, я бот, который поможет найти нужную информацию на портале вышки. Спроси меня о чем-нибудь и я " \
              "отвечу и найду ссылки :) "
 
-model = Model(True)
+MODEL = Model(True)
 
 
 # Bot conversation
-@bot.message_handler(commands=['start'])
-def send_start_keyboard(message, text=start_text):
-    msg = bot.send_message(message.from_user.id, text=text)
-    bot.register_next_step_handler(msg, callback_worker_start)
+@BOT.message_handler(commands=['start'])
+def send_start(message, text=start_text):
+    msg = BOT.send_message(message.from_user.id, text=text)
+    BOT.register_next_step_handler(msg, callback_worker_start)
 
 
 # Returning answer for the question function
 def callback_worker_start(call):
     text = ' '.join(tokenize_data(call.text))
-    res, link = model.predict(text)
+    res, link = MODEL.predict(text)
     text_to_user = f'Скорее всего вы сможете найти ответ в этом тексте:\n{res}\n\nИли более подробно ознакомиться с ' \
                    f'этим по ссылке: \n{link} '
-    send_start_keyboard(call, text=text_to_user)
+    send_start(call, text=text_to_user)
 
 
-bot.polling(none_stop=True)
+BOT.polling(none_stop=True)
